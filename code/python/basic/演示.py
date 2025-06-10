726d035f29f94966910ae5193158920364e6510f
aming <hanyudeye@163.com>
2025-06-10 17:31:03 +0800

    w

---
+++ code/python/basic/演示.py
@@ -0,0 +1,13 @@
+# 敲木头
+
+mutou="敲木头"
+
+def qiaomutou():
+    print(mutou)
+
+def zifuchuanjia():
+    print("Hello" + " World")  # 字符串连接
+
+if __name__ == "__main__":  
+    # qiaomutou()
+    zifuchuanjia()
