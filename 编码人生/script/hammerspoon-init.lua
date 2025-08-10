53fc09a772c58a96507ccf58c1705aad95eac7b0
hanyudeye <hanyudeye8@gmail.com>
2025-08-10 10:28:08 +0800

    m

--- 编码人生/script/hammerspoon-init.lua
+++ 编码人生/script/hammerspoon-init.lua
@@ -1,5 +1,5 @@
 
--- hammerspoon 脚本
+-- hammerspoon 脚本，用于在 macOS 上实现快捷键功能
 
 -- 监听 Cmd+F，执行 killall say
 hs.hotkey.bind({"cmd"}, "F", function()
@@ -8,6 +8,7 @@ end)
 
 -- 激活名为“备忘录”的窗口
 hs.hotkey.bind({"cmd"}, ".", function()
+-- ，部分匹配窗口标题
     local win = hs.window.find("备忘录")
     if win then
         win:focus()
@@ -16,3 +17,19 @@ hs.hotkey.bind({"cmd"}, ".", function()
         hs.alert.show("未找到窗口：备忘录")
     end
 end)
+
+hs.hotkey.bind({"cmd"}, ",", function()
+--  按应用名查找第一个窗口
+    local app = hs.application.find("Emacs")
+    if app then
+        local win = app:mainWindow()
+        if win then
+            win:focus()
+            win:raise()
+        else
+             hs.alert.show("未找到窗口：Emacs")
+       end
+    end
+end)
+
+
