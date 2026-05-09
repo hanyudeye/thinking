07fac9f12f055a83de64735737a1dcd03f82e789
hanyudeye <hanyudeye8@gmail.com>
2026-05-09 17:20:30 +0800

    w

--- 技术/编码术/script/微信双开.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-start "" "D:\Program Files (x86)\Tencent\Weixin\Weixin.exe"
-start "" "D:\Program Files (x86)\Tencent\Weixin\Weixin.exe"
-
-exit
--- 计算机文档/scoop.md
+++
@@ -1,7 +0,0 @@
-# 安装
-
-# 重新安装软件
-
-scoop uninstall [soft]
-scoop install [soft]
-
